38e9f4b7460a0d5af7d9f6fccb0fe41429d72021
Eduardo Speroni <edusperoni@gmail.com>
2022-09-01 17:18:04 -0300

    add bug

--- app/search/search-page.ts
+++ app/search/search-page.ts
@@ -4,4 +4,10 @@ import { SearchViewModel } from './search-view-model'
 export function onNavigatingTo(args: NavigatedData) {
   const page = <Page>args.object
   page.bindingContext = new SearchViewModel()
+  
+}
+
+export function navigate(args: any) {
+  console.log('trying to navigate');
+  args.object.page.frame.navigate('browse/browse-page');
 }
